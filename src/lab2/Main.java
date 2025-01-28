03587c6e6339252474583ad6fab264c5984d81cf
Newide <m.snitynska@gmail.com>
2025-01-28 10:12:12 +0200

    vnb

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -3,7 +3,7 @@ package lab2;
 public class Main {
     public static void main(String[] arg) {
         int counter;
-        counter = 99;
+        counter = 109;
         System.out.println(counter);
     }
 
